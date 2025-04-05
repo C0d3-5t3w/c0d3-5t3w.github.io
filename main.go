372fc782cb6f48fb1cd47045aa1be9b1fff82528
C0d3-5t3w <stewy1994@gmail.com>
2025-04-05 07:57:10 -0700

    Update

--- main.go
+++ main.go
@@ -10,26 +10,21 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("."))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := filepath.Clean(r.URL.Path)
-
 		if path == "/" {
 			http.ServeFile(w, r, "index.html")
 			return
 		}
-
 		if strings.HasPrefix(path, "/pages/") {
 			http.ServeFile(w, r, path[1:])
 			return
 		}
-
 		fs.ServeHTTP(w, r)
 	})
-
-	port := ":8080"
-	fmt.Printf("Server starting on http://localhost%s\n", port)
-	err := http.ListenAndServe(port, nil)
+	var Addr string
+	fmt.Scanln("Type an address to serve from (E.g. 127:0.0.1:8080):", Addr)
+	err := http.ListenAndServe(Addr, nil)
 	if err != nil {
 		log.Fatal("Server failed to start: ", err)
 	}
